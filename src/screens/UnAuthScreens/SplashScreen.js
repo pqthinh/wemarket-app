60558dd981c615b9c9ca5641a6bff60a3e310c4a
pqthinh <thinhpq@its-global.vn>
2021-10-21 22:19:42 +0700

    splash screen

--- src/screens/UnAuthScreens/SplashScreen.js
+++ src/screens/UnAuthScreens/SplashScreen.js
@@ -1,60 +1,103 @@
-import React from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
-import PropTypes from 'prop-types'
-import AppIntroSlider from 'react-native-app-intro-slider'
+import { useTheme } from '@react-navigation/native'
+import { Button, Text } from '@ui-kitten/components'
 import { IMAGES } from 'assets'
+import PropTypes from 'prop-types'
+import React from 'react'
+import {
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  SafeAreaView
+} from 'react-native'
+import AppIntroSlider from 'react-native-app-intro-slider'
+import Icon from 'react-native-vector-icons/Feather'
 
 const slides = [
   {
     key: 1,
-    title: 'Title 1',
-    text: 'Description.\nSay something cool',
-    image: IMAGES.SPLASH1_IMAGE,
-    backgroundColor: '#59b2ab'
+    title: '',
+    text: '',
+    image: IMAGES.SPLASH1_IMAGE
   },
   {
     key: 2,
-    title: 'Title 2',
-    text: 'Other cool stuff',
-    image: IMAGES.SPLASH2_IMAGE,
-    backgroundColor: '#febe29'
+    title: '',
+    text: '',
+    image: IMAGES.SPLASH2_IMAGE
   },
   {
     key: 3,
-    title: 'Rocket guy',
-    text: "I'm already out of descriptions\n\nLorem ipsum bla bla bla",
-    image: IMAGES.SPLASH3_IMAGE,
-    backgroundColor: '#22bcb5'
+    title: '',
+    text: '',
+    image: IMAGES.SPLASH3_IMAGE
+  },
+  {
+    key: 4,
+    title: '',
+    text: '',
+    image: IMAGES.SPLASH4_IMAGE
   }
 ]
 
 export default function SplashScreen({ onDone }) {
+  const { colors } = useTheme()
+
   _renderItem = ({ item }) => {
     if (!item) return null
     return (
-      <View style={styles.slide}>
+      <ImageBackground style={styles.slide} source={item.image}>
         <Text style={styles.title}>{item.title}</Text>
-        <Image source={item.image} style={styles.image} />
         <Text style={styles.text}>{item.text}</Text>
-      </View>
+      </ImageBackground>
     )
   }
   _renderNextButton = () => {
     return (
       <View style={styles.buttonCircle}>
-        <Icon
-          name='md-arrow-round-forward'
-          color='rgba(255, 255, 255, .9)'
-          size={24}
-        />
+        <Text style={styles.nextButton}>Tiếp theo</Text>
+        <Icon name='arrow-right' color={colors.primary} size={24} />
       </View>
     )
   }
   _renderDoneButton = () => {
     return (
       <View style={styles.buttonCircle}>
-        <Icon name='md-checkmark' color='rgba(255, 255, 255, .9)' size={24} />
+        <Button>Khám phá ứng dụng WeMarket</Button>
+        <Icon name='external-link' color={colors.primary} size={24} />
+      </View>
+    )
+  }
+  _renderPagination = activeIndex => {
+    return (
+      <View style={styles.paginationContainer}>
+        <SafeAreaView>
+          <View style={styles.paginationDots}>
+            {slides.length > 1 &&
+              slides.map((_, i) => (
+                <TouchableOpacity
+                  key={i}
+                  style={[
+                    styles.dot,
+                    i === activeIndex
+                      ? { backgroundColor: 'white' }
+                      : { backgroundColor: 'rgba(0, 0, 0, .2)' }
+                  ]}
+                  onPress={() => this.slider?.goToSlide(i, true)}
+                />
+              ))}
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: '#023e3f' }]}
+            >
+              <Text style={styles.buttonText}>Log in</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
       </View>
     )
   }
@@ -62,9 +105,13 @@ export default function SplashScreen({ onDone }) {
     <AppIntroSlider
       renderItem={_renderItem}
       data={slides}
+      nextButton={_renderNextButton}
+      renderDoneButton={_renderDoneButton}
+      renderNextButton={_renderNextButton}
+      renderPagination={_renderPagination}
       onDone={onDone}
       showSkipButton={true}
-      style={{ backgroundColor: 'rgba(255, 255, 255, 0.9)' }}
+      style={{ backgroundColor: colors.background }}
     />
   )
 }
@@ -77,12 +124,77 @@ const styles = StyleSheet.create({
   buttonCircle: {
     width: 40,
     height: 40,
-    backgroundColor: 'rgba(0, 0, 0, .2)',
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center'
   },
-  image: {
-    objectFit: 'contain'
+  slide: { flex: 1, resizeMode: 'cover', opacity: 0.9 },
+  title: {
+    marginTop: 92,
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 14,
+    textAlign: 'center'
+  },
+  paginationContainer: {
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+    right: 16
+  },
+  paginationDots: {
+    height: 16,
+    margin: 16,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 4
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 24
+  },
+  button: {
+    flex: 1,
+    paddingVertical: 20,
+    marginHorizontal: 8,
+    borderRadius: 24,
+    backgroundColor: '#1cb278'
+  },
+  buttonText: {
+    color: 'red',
+    fontWeight: '600',
+    textAlign: 'center'
   }
 })
+// {
+//   key: 1,
+//   title: 'Xin chào',
+//   text: 'Đăng ký thành viên để gia nhập nhóm WeMarket',
+//   image: IMAGES.SPLASH1_IMAGE
+// },
+// {
+//   key: 2,
+//   title: 'Cập nhật thông tin khuyến mãi mỗi ngày',
+//   text: 'Đi chợ tiết kiệm hơn với nhiều mặt hàng khuyến mãi mỗi ngày ',
+//   image: IMAGES.SPLASH2_IMAGE
+// },
+// {
+//   key: 3,
+//   title: 'Mua hàng dễ dàng',
+//   text: 'Hỗ trợ chi phí giao hàng cho đơn hàng từ 1.000.000vnd trong phạm vi 10km ',
+//   image: IMAGES.SPLASH3_IMAGE
+// },
+// {
+//   key: 4,
+//   title: 'Tìm kiếm sản phẩm nhanh chóng',
+//   text: 'Đề xuất sản phẩm , tìm kiếm sảm phẩm theo địa điểm phạm vi phù hợp với bạn nhất ',
+//   image: IMAGES.SPLASH3_IMAGE
+// }
