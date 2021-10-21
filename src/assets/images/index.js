8705fd4bf056da288f4737eefe036d257b9b41fc
pqthinh <thinhpq@its-global.vn>
2021-10-21 22:19:42 +0700

    splash screen

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -8,7 +8,8 @@ export default {
   NO_CONTENT: require('./seo.png'),
   FAIL_IMAGE: require('./time.png'),
   LOADING: LOADING,
-  SPLASH1_IMAGE: require('./splash1.jpg'),
-  SPLASH2_IMAGE: require('./splash2.jpg'),
-  SPLASH3_IMAGE: require('./splash3.jpg')
+  SPLASH1_IMAGE: require('./sp1.png'),
+  SPLASH2_IMAGE: require('./sp2.png'),
+  SPLASH3_IMAGE: require('./sp3.png'),
+  SPLASH4_IMAGE: require('./sp4.png')
 }
