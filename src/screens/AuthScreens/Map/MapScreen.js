8606ba052069c96a0db2a8935ee4ba15f9e3dc29
giapdz <giapdz104@gmail.com>
2021-10-26 03:50:15 +0700

    update set radius

--- src/screens/AuthScreens/Map/MapScreen.js
+++ src/screens/AuthScreens/Map/MapScreen.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from "react"
-import { Alert, SafeAreaView, StatusBar, StyleSheet} from "react-native"
+import React, { useEffect, useState, useMemo } from "react"
+import { Alert, SafeAreaView, StatusBar, StyleSheet, TouchableOpacity} from "react-native"
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps"
 import { check, request, PERMISSIONS, RESULTS } from "react-native-permissions"
 import Geolocation from "react-native-geolocation-service"
-import locations from "./data"
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import products from "./data"
 import ic_phone from '../../../assets/images/ic_phone.png'
 import MapModal from "../../../components/MapModal"
+import SettingModal from "../../../components/SettingModal"
 // import { customStyleMap} from "./styled"
 const MapScreen = ({ navigation }) => {
   const [location, setLocation] = useState(null)
+  // const [zoom, setZoom] = useState('')
   const [modalVisible, setModalVisible] = useState(false);
-  const [product, setProduct] =useState({place:''})
+  const [modalVisible2, setModalVisible2] = useState(false);
+  const [product, setProduct] =useState({place:'', name:'', image:'',name_user:'', star:'',price:''})
+  const [radius,setRadius]= useState(1);
+  const [region, setRegion] = useState({
+    latitude: location?.latitude || 21.0369,
+    longitude: location?.longitude || 105.7823,
+    latitudeDelta: Math.PI*radius/ 111.045,
+    longitudeDelta: 0.01
+  });
   const handleLocationPermission = async () => { // 👈
     let permissionCheck = '';
     if (Platform.OS === 'ios') {
@@ -48,10 +59,15 @@ const MapScreen = ({ navigation }) => {
   const close = ()=> {
     setModalVisible(false)
   }
+  const close_2 = ()=> {
+    setModalVisible2(false)
+  }
   useEffect(() => {
     handleLocationPermission()
   }, [])
-
+  useMemo(() => {
+    setRegion(prevState=>({...prevState, latitudeDelta:Math.PI*radius/ 111.045}))
+  }, [radius])
   useEffect(() => {
     Geolocation.getCurrentPosition(
       position => {
@@ -74,13 +90,9 @@ const MapScreen = ({ navigation }) => {
         <MapView
           style={styles.map}
           provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: location?.latitude || 21.0541883 ,
-            longitude: location?.longitude || 105.8263367,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          
+          region={region}
+          // onRegionChange={region => setRegion(region)}
+       
         showsIndoors={true}
         zoomControlEnabled={true}
         zoomEnabled={true}
@@ -98,21 +110,22 @@ const MapScreen = ({ navigation }) => {
           loadingIndicatorColor="#fcb103" 
           loadingBackgroundColor="#242f3e" 
         >
-          {locations.map((host, i) => {
-            if (host.latitude && host.longitude) {
-              console.log("TEST", host.latitude);
+          {products.map((host, i) => {
+            if (host.place.latitude && host.place.longitude) {
+              
              return(<Marker
                 key={i}
                 coordinate={{
-                  latitude: host.latitude,
-                  longitude: host.longitude
+                  latitude: host.place.latitude,
+                  longitude: host.place.longitude
                 }}
                 image={ic_phone}
-                title={host.name}
+                title={host.name_product}
                 pinColor={"#ffd1dc"}
                 onPress={()=> {
                   setModalVisible(true);
-                  setProduct({place: host.name})
+                  setProduct({place: host.place.name, name:host.name_product,image:host.product_images[0],name_user:host.name_user,star:host.star, price:host.price})
+                  setModalVisible2(false)
                 }}
               />)
             }
@@ -122,13 +135,17 @@ const MapScreen = ({ navigation }) => {
           latitude: location?.latitude || 21.0541883,
           longitude: location?.longitude || 105.8263367,
         }}
-        radius={10000}
+        radius={radius*1000}
         strokeWidth={2}
         strokeColor="#3399ff"
         fillColor="rgba(128,191,255,0.2)"
       />
           </MapView>
+          <TouchableOpacity style={styles.Button} onPress={()=>{setModalVisible2(true); setModalVisible(false)}}>
+          <Ionicons name="options" size={24} color="black" />
+          </TouchableOpacity>
           <MapModal modalVisible={modalVisible} close={close} product={product}/>
+          <SettingModal modalVisible={modalVisible2} close={close_2} sliderValue={radius} setSliderValue={setRadius}/>
       {/* )} */}
     </SafeAreaView>
   )
@@ -143,6 +160,25 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  Button: {
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    position: 'absolute',
+    top: 60,
+    right: 13.5,
+    width: 37,
+    height: 36,
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  }
 })
 
 export default MapScreen
