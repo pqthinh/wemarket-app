09ce6b7bcb56481f28b22bdb0fc7360e85866008
giapdz <giapdz104@gmail.com>
2021-11-15 20:24:12 +0700

    update loading map

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,20 +1,24 @@
 import useCache from 'hooks/useCache'
 import React, { useEffect, useMemo, useState } from 'react'
 import {
+  ActivityIndicator,
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  TouchableOpacity
+  TouchableOpacity,
+  View
 } from 'react-native'
 import Geolocation from 'react-native-geolocation-service'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions'
 import MapModal from '../../components/MapModal'
 import SettingModal from '../../components/SettingModal'
 import { GOOGLE_MAPS_API_KEY } from '../../utils/map/constants'
 import products from './data'
 const MapScreen = () => {
+  const [loading, setLoading] = useState(true)
   const [location, setLocation] = useState({
     latitude: 21.0369,
     longitude: 105.7823
@@ -43,15 +47,51 @@ const MapScreen = () => {
     longitude: null
   })
   const [openDirection, setOpenDirection] = useState(false)
+  const handleLocationPermission = async () => {
+    let permissionCheck = ''
+    if (Platform.OS === 'ios') {
+      permissionCheck = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
 
+      if (
+        permissionCheck === RESULTS.BLOCKED ||
+        permissionCheck === RESULTS.DENIED
+      ) {
+        const permissionRequest = await request(
+          PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        )
+        permissionRequest === RESULTS.GRANTED
+          ? console.warn('Location permission granted.')
+          : console.warn('location permission denied.')
+      }
+    }
+
+    if (Platform.OS === 'android') {
+      permissionCheck = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+
+      if (
+        permissionCheck === RESULTS.BLOCKED ||
+        permissionCheck === RESULTS.DENIED
+      ) {
+        const permissionRequest = await request(
+          PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+        )
+        permissionRequest === RESULTS.GRANTED
+          ? console.warn('Location permission granted.')
+          : console.warn('location permission denied.')
+      }
+    }
+  }
   const close = () => {
     setModalVisible(false)
   }
   const close_2 = () => {
     setModalVisible2(false)
   }
+  useEffect(() => {
+    handleLocationPermission()
+  }, [])
   useEffect(async () => {
-    setRadius(await get('save_radius'))
+    setRadius((await get('save_radius')) || 1)
   }, [])
 
   useMemo(() => {
@@ -66,6 +106,7 @@ const MapScreen = () => {
         console.log(position)
         const { latitude, longitude } = position.coords
         setLocation({ latitude, longitude })
+        setLoading(false)
         setRegion(prevState => ({
           ...prevState,
           latitude: latitude,
@@ -74,122 +115,119 @@ const MapScreen = () => {
       },
       error => {
         console.log(error.code, error.message)
+        setLoading(false)
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     )
   }, [])
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle='dark-content' />
-      {/* {location && ( */}
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        region={region}
-        //region={region}
-        //onRegionChangeComplete={region => setRegion(region)}
-
-        showsIndoors={true}
-        zoomControlEnabled={true}
-        zoomEnabled={true}
-        zoomTapEnabled={true}
-        showsScale={true}
-        showsBuildings={true}
-        showsCompass={true}
-        showsUserLocation={true}
-        // customMapStyle={customStyleMap}
-        paddingAdjustmentBehavior='automatic'
-        showsMyLocationButton={true}
-        showsBuildings={true}
-        maxZoomLevel={17.5}
-        loadingEnabled={true}
-      >
-        {products.map((host, i) => {
-          if (host.place.latitude && host.place.longitude) {
-            return (
-              <Marker
-                key={i}
-                coordinate={{
-                  latitude: host.place.latitude,
-                  longitude: host.place.longitude
-                }}
-                image={{
-                  uri: 'https://i.ibb.co/6DxQH3t/ic-phone.png'
-                }}
-                title={host.name_product}
-                pinColor={'#ffd1dc'}
-                onPress={() => {
-                  setModalVisible(true)
-                  setProduct({
-                    place: host.place.name,
-                    name: host.name_product,
-                    image: host.product_images[0],
-                    name_user: host.name_user,
-                    star: host.star,
-                    price: host.price
-                  })
-                  setModalVisible2(false)
-                  setCoordinate({
+  if (loading) {
+    return (
+      <View style={styles.spinnerView}>
+        <ActivityIndicator size='large' color='#E26740' />
+      </View>
+    )
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle='dark-content' />
+        {/* {location && ( */}
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          region={region}
+          // zoomControlEnabled={true}
+          // zoomEnabled={true}
+          // zoomTapEnabled={true}
+          showsUserLocation={true}
+          // customMapStyle={customStyleMap}
+          //maxZoomLevel={17.5}
+        >
+          {products.map((host, i) => {
+            if (host.place.latitude && host.place.longitude) {
+              return (
+                <Marker
+                  key={i}
+                  coordinate={{
                     latitude: host.place.latitude,
                     longitude: host.place.longitude
-                  })
-                  setRegion(prevState => ({
-                    ...prevState,
-                    latitude: host.place.latitude,
-                    longitude: host.place.longitude
-                  }))
-                  setOpenDirection(false)
-                }}
-              />
-            )
-          }
-        })}
-        {openDirection && (
-          <MapViewDirections
-            origin={location}
-            destination={coordinate}
-            apikey={GOOGLE_MAPS_API_KEY}
-            strokeWidth={4}
-            strokeColor='#197CFF'
+                  }}
+                  image={{
+                    uri: 'https://i.ibb.co/6DxQH3t/ic-phone.png'
+                  }}
+                  title={host.name_product}
+                  pinColor={'#ffd1dc'}
+                  onPress={() => {
+                    setModalVisible(true)
+                    setProduct({
+                      place: host.place.name,
+                      name: host.name_product,
+                      image: host.product_images[0],
+                      name_user: host.name_user,
+                      star: host.star,
+                      price: host.price
+                    })
+                    setModalVisible2(false)
+                    setCoordinate({
+                      latitude: host.place.latitude,
+                      longitude: host.place.longitude
+                    })
+                    setRegion(prevState => ({
+                      ...prevState,
+                      latitude: host.place.latitude,
+                      longitude: host.place.longitude
+                    }))
+                    setOpenDirection(false)
+                  }}
+                />
+              )
+            }
+          })}
+          {openDirection && (
+            <MapViewDirections
+              origin={location}
+              destination={coordinate}
+              apikey={GOOGLE_MAPS_API_KEY}
+              strokeWidth={4}
+              strokeColor='#197CFF'
+            />
+          )}
+          {coordinate.latitude !== null && <Marker coordinate={coordinate} />}
+          <MapView.Circle
+            center={{
+              latitude: location?.latitude || 21.0541883,
+              longitude: location?.longitude || 105.8263367
+            }}
+            radius={radius * 1000}
+            strokeWidth={2}
+            strokeColor='#3399ff'
+            fillColor='rgba(128,191,255,0.2)'
           />
-        )}
-        {coordinate.latitude !== null && <Marker coordinate={coordinate} />}
-        <MapView.Circle
-          center={{
-            latitude: location?.latitude || 21.0541883,
-            longitude: location?.longitude || 105.8263367
+        </MapView>
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={() => {
+            setModalVisible2(true)
+            setModalVisible(false)
           }}
-          radius={radius * 1000}
-          strokeWidth={2}
-          strokeColor='#3399ff'
-          fillColor='rgba(128,191,255,0.2)'
+        >
+          <Ionicons name='options' size={24} color='black' />
+        </TouchableOpacity>
+        <MapModal
+          modalVisible={modalVisible}
+          close={close}
+          product={product}
+          setOpenDirection={setOpenDirection}
         />
-      </MapView>
-      <TouchableOpacity
-        style={styles.Button}
-        onPress={() => {
-          setModalVisible2(true)
-          setModalVisible(false)
-        }}
-      >
-        <Ionicons name='options' size={24} color='black' />
-      </TouchableOpacity>
-      <MapModal
-        modalVisible={modalVisible}
-        close={close}
-        product={product}
-        setOpenDirection={setOpenDirection}
-      />
-      <SettingModal
-        modalVisible={modalVisible2}
-        close={close_2}
-        sliderValue={radius}
-        setSliderValue={setRadius}
-      />
-      {/* )} */}
-    </SafeAreaView>
-  )
+        <SettingModal
+          modalVisible={modalVisible2}
+          close={close_2}
+          sliderValue={radius}
+          setSliderValue={setRadius}
+        />
+        {/* )} */}
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -199,6 +237,11 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject
+  },
+  spinnerView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   Button: {
     backgroundColor: 'rgba(255, 255, 255, 0.75)',
