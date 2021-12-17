d764987c5d3de4d8fe81d6d9333ef96067a5b3d6
giapdz <giapdz104@gmail.com>
2021-12-17 23:53:47 +0700

    update icon

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import { PersistGate } from 'redux-persist/integration/react'
 import * as eva from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
-import { MaterialIconsPack } from './material-icons'
-import { FeatherIconsPack } from './feather-icons'
 import { IonIconsPack } from './ion-icons'
 import { AppNavigator } from './src/AppNavigator'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
@@ -39,14 +37,7 @@ const App = props => {
     <Provider store={store}>
       <ThemeProvider theme={themeColors}>
         <PersistGate loading={<LoadingAtoms />} persistor={persistor}>
-          <IconRegistry
-            icons={[
-              EvaIconsPack,
-              MaterialIconsPack,
-              FeatherIconsPack,
-              IonIconsPack
-            ]}
-          />
+          <IconRegistry icons={[EvaIconsPack, IonIconsPack]} />
           <AppProvider>
             <ThemeContext.Provider
               value={{ theme: theme, toggleTheme: () => toggleTheme() }}
